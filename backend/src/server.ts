f1ddfda7d139dffedd82959f6ed8b68017a155f5
Saajid Ahamed <saajidahamed1@gmail.com>
2024-12-21 21:31:23 +0530

    added api key setup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,3 +1,18 @@
 // Server Code Starts here
-let num: number = 123;
-console.log(num);
+import express from 'express';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+const apiKey = process.env.API_KEY; // store your API Key in a .env file in this format: " API_KEY= "ENTER YOUR KEY HERE" "
+
+let app = express();
+const port: number = 7000;
+
+app.get("/", (req, res) => {
+    res.send("Connection established to the Node.JS backend Successfully");
+})
+
+// listen for http requests
+app.listen(port, ()=> {
+    console.log(`Server listening on port ${port}`);
+})
