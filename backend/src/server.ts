639d2dd8cfff8533f1387ae2b98b01d56e1aef24
Saajid Ahamed <saajidahamed1@gmail.com>
2024-12-22 00:49:06 +0530

    integrated controller and service

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,18 +1,4 @@
-// Server Code Starts here
-import express from 'express';
-import * as dotenv from 'dotenv';
+import {MainController} from "./MainController";
 
-dotenv.config();
-const apiKey = process.env.API_KEY; // store your API Key in a .env file in this format: " API_KEY= "ENTER YOUR KEY HERE" "
-
-let app = express();
-const port: number = 7000;
-
-app.get("/", (req, res) => {
-    res.send("Connection established to the Node.JS backend Successfully");
-})
-
-// listen for http requests
-app.listen(port, ()=> {
-    console.log(`Server listening on port ${port}`);
-})
+const mainController: MainController = new MainController(7000);
+mainController.start();
