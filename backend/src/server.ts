06aa685f8a189db0c9c01a172b2ec6b57762248e
Saajid Ahamed <saajidahamed1@gmail.com>
2024-12-22 20:25:50 +0530

    added dependency

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import {MainController} from "./MainController";
 import {MainService} from "./MainService";
+import {LLMService} from "./LLMService";
 // initialize the RESTful Controller to handle HTTP Requests
-new MainController(7000, new MainService()).start();
+new MainController(7000, new MainService(new LLMService())).start();
