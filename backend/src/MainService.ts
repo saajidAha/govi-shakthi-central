d8de8b648fff1a18d871c54564c248448ba30e87
Saajid Ahamed <saajidahamed1@gmail.com>
2024-12-22 20:26:24 +0530

    added functionality to acces the LLM

--- backend/src/MainService.ts
+++ backend/src/MainService.ts
@@ -1,11 +1,18 @@
-export class MainService{
+import {LLMService} from "./LLMService";
 
-    getPricePrediction( fruitName: string): string {
-        return fruitName;
+export class MainService{
+    readonly llmService: LLMService;
+
+    constructor(llmService: LLMService) {
+        this.llmService = llmService;
     }
 
-    getAlternativeProductSuggestion( fruitName: string ): string {
-        return fruitName;
+    async getPricePrediction( fruitName: string): Promise<string>{
+        return await this.llmService.computeResponse("Generate a price prediction for " + fruitName + ". You can use a fake dataset or assume some fake data. The country is USA. make a prediction. structure your response in json format with just the importannt info regarding the dataset and prices. keep it simple. do not generate long descriptions.");
+    }
+
+    async getAlternativeProductSuggestion( fruitName: string ): Promise<string> {
+        return await this.llmService.computeResponse("Generate a List of creative alternative product suggestions for this given product: " + fruitName +". The goal is to help farmers to create a value addition to their fruits and sell them at a profit. Keep the response short and simple");
     }
 
     getMarketPlaceRecommendation( product: string): string[]{
