d551c6f4bdee1eaeea49c27b43a6c61607e416da
Saajid Ahamed <saajidahamed1@gmail.com>
2024-12-24 18:37:29 +0530

    integrated the dataset to be used by all the features and to return specifc values. added comments.

--- backend/src/MainService.ts
+++ backend/src/MainService.ts
@@ -1,28 +1,63 @@
 import {LLMService} from "./LLMService";
+import fruit_dataset from "./../datasets/sri_lanka_fruit_data.json";
+import market_dataset from "./../datasets/fruit_marketplace_data.json";
+import raw_material_market_dataset from "./../datasets/fruit_raw_material_marketplace_data.json";
 
+/**
+ * Main Service class that handles the delegation and execution of all services and business logic
+ */
 export class MainService{
-    readonly llmService: LLMService;
+    private llmService: LLMService;
 
+    /**
+     * Constructor to initalize service class
+     * @param llmService service object to be used to contact the LLM
+     */
     constructor(llmService: LLMService) {
         this.llmService = llmService;
     }
 
-    async getPricePrediction( fruitName: string): Promise<string>{
-        return await this.llmService.computeResponse("Generate a price prediction for " + fruitName + ". You can use a fake dataset or assume some fake data. The country is USA. make a prediction. structure your response in json format with just the importannt info regarding the dataset and prices. keep it simple. do not generate long descriptions.");
+    /**
+     * Predicts prices based on the existing dataset and provides insights
+     * @param fruit name of the fruit
+     */
+    async getAllPricePrediction(fruit: string): Promise<string>{
+        return await this.llmService.computeResponse(`Analyse the contents of the json and give me a price prediction for every single fruit type in the json. include the fruit type, retail price (in LKR), whole sale price (in LKR), predicted prices, predicted time range, prediction factors considered and other insights about prediction in json format as an array of objects. do not give me any other unnecessary info: \n${JSON.stringify(fruit_dataset)}\n.`);
     }
 
-    async getAlternativeProductSuggestion( fruitName: string ): Promise<string> {
-        return await this.llmService.computeResponse("Generate a List of creative alternative product suggestions for this given product: " + fruitName +". The goal is to help farmers to create a value addition to their fruits and sell them at a profit. Keep the response short and simple");
+    /**
+     * Suggests alternative products based on the specified fruit
+     * @param fruit name of the fruit
+     * @return json object
+     */
+    getAlternativeProductSuggestion( fruit: string ){
+        return fruit_dataset.filter(item => item.fruit_type === fruit);
     }
 
-    getMarketPlaceRecommendation( product: string): string[]{
-        return [product];
+    /**
+     * Recommends marketplaces to sell the alternative products based on the specified product
+     * @param product alternative product based on the dataset
+     * @return json object
+     */
+    getMarketPlaceRecommendation( product: string ){
+        return market_dataset.filter(item => item.alternative_product === product);
     }
 
-    getRawMaterialMarketPlace( product: string ): string[]{
-        return [product];
+    /**
+     * Recommend ingredients, raw material marketplaces to purchase ingredient to produce the desired alternative product
+     * @param product alternative product based on the dataset
+     * @return json object
+     */
+    getRawMaterialMarketPlace( product: string ){
+        return raw_material_market_dataset.filter(item => item.alternative_product === product);
     }
 
+    /**
+     * FEATURE STILL IN DEVELOPMENT
+     * Registers a user
+     * @param username username
+     * @param password password
+     */
     registerUser( username: string, password: string): boolean{
         return false;
     }
