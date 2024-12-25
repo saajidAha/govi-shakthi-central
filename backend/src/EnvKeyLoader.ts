384b7cd3fc5c8f8e89215fb7dbca0ee85c94d436
Saajid Ahamed <saajidahamed1@gmail.com>
2024-12-26 05:00:53 +0530

    created module to load .env files

--- backend/src/EnvKeyLoader.ts
+++ backend/src/EnvKeyLoader.ts
@@ -1,11 +1,7 @@
 import dotenv from 'dotenv';
 /**
- * Utility class that loads the private keys from .env files;
- * .env files containing (API Keys or any other private keys) will not be pushed to git.
- * Request the keys from the developer of this code if you require it.
+ * Loads the keys from .env files;
  */
-export class EnvKeyLoader{
-    static {dotenv.config();}
-    static readonly API_KEY: string = String(process.env.API_KEY);
-    static readonly MONGODB_URI: string = String(process.env.MONGODB_URI);
-}
+dotenv.config();
+export let API_KEY: string = String(process.env.API_KEY);
+export let MONGODB_URI = String(process.env.MONGODB_URI);
