841717e3730f869ca844b253f429005c3ffce50b
Saajid Ahamed <saajidahamed1@gmail.com>
2024-12-28 23:17:42 +0530

    removed credential model as mongoose is removed

--- backend/src/models/CredentialModel.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const credentialSchema = new mongoose.Schema({
-        username: String,
-        password: String
-    })
-const credentialModel = mongoose.model("Login", credentialSchema);
-
-export default credentialModel;
