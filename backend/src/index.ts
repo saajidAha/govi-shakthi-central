5fd0af7562eb210c8ffb049b53f877273a4e91da
Saajid Ahamed <saajidahamed1@gmail.com>
2025-01-20 18:09:33 +0530

    created entrypoint for the application

---
+++ backend/src/index.ts
@@ -0,0 +1,10 @@
+import { Server } from "./services/Server"
+import { Controller } from "./controllers/Controller";
+import { Repository } from "./repositories/Repository";
+import { LLMService } from "./services/LLMService";
+
+// Inject controller with required dependencies
+const mainController: Controller = Controller.getInstance(Repository.getInstance(Server.MONGODB_URI), LLMService.getInstance(Server.API_KEY));
+
+// Inject controller dependency and start the server
+Server.getInstance(mainController).start()
