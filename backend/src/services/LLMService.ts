cf27c5933536575acaa208e545c26763aebaa889
Saajid Ahamed <saajidahamed1@gmail.com>
2025-01-20 18:02:09 +0530

    encapsulated prompts within the class, added format methods to sanitize llm responses to valid json

--- backend/src/services/LLMService.ts
+++ backend/src/services/LLMService.ts
@@ -1,18 +1,30 @@
 import {GenerativeModel, GoogleGenerativeAI} from "@google/generative-ai"
-import {API_KEY} from "./EnvLoaderService";
+import { Repository } from "../repositories/Repository";
 /**
  * Class responsible for communication with the Google Gemini API
  */
 export class LLMService {
-    // private static API_KEY: string = EnvKeyLoader.API_KEY;
-    private static model: GenerativeModel = LLMService.initModel();
+    private model: GenerativeModel;
+    private API_KEY: string;
+    private static instance: LLMService;
 
+    // Enforces Singleton pattern
+    private constructor(apiKey: string){
+        this.API_KEY = apiKey;
+        this.model = this.initModel();
+    }
+
+    // Able to create only a single instance for a class
+    public static getInstance(apiKey: string){
+        LLMService.instance = LLMService.instance ?? new LLMService(apiKey);
+        return LLMService.instance;
+    }
     /**
      * Initializes LLM model with API key
      * @return GenerativeModel object
      */
-    static initModel(): GenerativeModel{
-        const genAI = new GoogleGenerativeAI(API_KEY);
+    public initModel(): GenerativeModel{
+        const genAI = new GoogleGenerativeAI(this.API_KEY);
         return genAI.getGenerativeModel({model: "gemini-1.5-flash-8b"});
     }
 
@@ -20,15 +32,29 @@ export class LLMService {
      * Contacts the Gemini API and retruns a Promise based on the prompt
      * @param prompt The prompt message to send to the LLM
      */
-    async computeResponse(prompt: string): Promise<string>{
+    public async fetchLLMResponse(prompt: string): Promise<string>{
         try{
-            const result = await LLMService.model.generateContent(prompt);
-            let response: string = result.response.text();
-            console.log(response);
-            return response;
+            const result = await this.model.generateContent(prompt);
+            return this.formatToJson( result.response.text() );
         }catch (error){
-            console.log("Error while communication with the LLM")
+            console.log("Error while communication with the LLM", error)
             return "error";
         }
     }
-}
+
+    /**
+     * Predicts prices based on the existing dataset and provides insights
+     */
+    public async fetchPricePrediction(repo: Repository): Promise<string>{
+        let fruit_data = await repo.findAll();
+        return await this.fetchLLMResponse(`Analyse the contents of the JSON and give me a price prediction for every single fruit type in the JSON.  Include: - fruitType - retailPrice (in LKR) - wholesalePrice (in LKR) - predictedPrices - predictedTimeRange - predictionFactors Format the response strictly as valid JSON, without any extra text or explanations. : \n${JSON.stringify(fruit_data)}\n.`);
+    }
+
+    /**
+     * Removes unnecessary symbols from response returned from the llm & Formats and parses it to JSON
+     * @param value string literal
+     */
+    public formatToJson(value: string){
+        return JSON.parse( value.replace(/```/g, "").replace(/json/g, "") );
+    }
+ }
