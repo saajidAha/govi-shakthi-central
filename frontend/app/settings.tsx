9125909160ae74c71d142048aea7614135c377ee
Sasindu Janapriya <sasindujanapriya2002@gmail.com>
2025-03-06 22:46:44 +0530

    connect agri-profile page

--- frontend/app/settings.tsx
+++ frontend/app/settings.tsx
@@ -24,7 +24,7 @@ export default function SettingsScreen() {
     { 
       title: 'Agri Profile', 
       description: 'Manage your Farming information',
-      onPress: () => router.push('/WelcomeScreen')
+      onPress: () => router.push('/agri-profile')
     },
     { 
       title: 'Privacy', 
