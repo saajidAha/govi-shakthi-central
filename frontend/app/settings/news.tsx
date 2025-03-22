5f9afd55fd773694aefaf340e8ca0c5a8a30d5c2
kanchanay25 <yatawarakanchana@gmail.com>
2025-03-22 22:15:43 +0530

    Add header section for the page.

--- frontend/app/settings/news.tsx
+++ frontend/app/settings/news.tsx
@@ -1,10 +1,99 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+  SafeAreaView,
+  Platform,
+  Image,
+  StatusBar,
+} from 'react-native';
+import { useRouter } from 'expo-router';
 
 export default function News() {
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>news</Text>
-    </View>
-  )
+    <SafeAreaView style={styles.safeArea}>
+      <StatusBar barStyle="light-content" />
+      <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+      	<View style={styles.header}>
+        <TouchableOpacity style={styles.backButton} onPress={() => router.back()}>
+          <Image source={require('../../assets/images/back.png')} style={styles.icon}/>
+        </TouchableOpacity>
+        <View style={styles.titleContainer}>
+          <Text style={styles.headerTitle}>GOVISHAKTHI</Text>
+          <Text style={styles.subTitle}>The country's leading farmer information center</Text>
+        </View>
+      </View>
+          
+        
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#000',
+    paddingTop: Platform.OS === 'android' ? 25 : 0,
+  },
+
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+  },
+
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+    backgroundColor: '#00A67E',
+
+  },
+
+   headerTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#000',
+    marginLeft: 15,
+  },
+
+  subTitle: {
+    fontSize: 14,
+    color: '#d0f0e8',
+    marginTop: 4,
+    paddingVirtical : 20,
+  },
+
+  dateTimeBar: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 10,
+  },
+
+  currentTimeText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+
+  dateText: {
+    fontSize: 14,
+    color: '#fff',
+  },
+
+  icon: {
+    width: 24,
+    height: 24,
+    tintColor: '#000000',
+  },
+
+  backButton: {
+    padding: 5,
+  },
+});
