f9c7c228e98b79955edaf1b3ec2d426e8471377c
DulenL30 <dulenvinjaya21@gmail.com>
2025-03-23 00:36:33 +0530

    Add basic file structure for the payment gateway.

--- frontend/app/subsciption/paymentGateway.tsx
+++ frontend/app/subsciption/paymentGateway.tsx
@@ -1,10 +1,16 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
+
 export default function paymentGateway() {
+
+
+    
   return (
     <View>
+
       <Text>paymentGateway</Text>
+
     </View>
   )
 }
