2af8a4dd8a55931c73296237a35405f447c25a36
DulenL30 <dulenvinjaya21@gmail.com>
2025-03-22 14:35:01 +0530

    Implement header section.

--- frontend/app/subsciption/subscriptionPlan.tsx
+++ frontend/app/subsciption/subscriptionPlan.tsx
@@ -1,10 +1,62 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+  SafeAreaView,
+  Platform,
+  Image,
+} from 'react-native';
+import { useRouter } from 'expo-router';
 
 export default function SubscriptionPlan() {
+  const router = useRouter();
   return (
-    <View>
-      <Text>subscriptionPlan</Text>
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+        <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+        <View style={styles.header}>
+          <TouchableOpacity style={styles.backButton} onPress={() => router.back()}>
+            <Text style={styles.backButtonText}>‹</Text>
+          </TouchableOpacity>
+          <Text style={styles.headerTitle}>Explore Premium</Text>
+        </View>
+        </ScrollView>
+    </SafeAreaView>
   )
 }
+
+
+
+const styles = StyleSheet.create({
+safeArea: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: Platform.OS === 'android' ? 25 : 0,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    backgroundColor: '#5BC8A8',
+  },
+  backButton: {
+    marginRight: 10,
+  },
+  backButtonText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#000',
+  },
+  headerTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#000',
+  },
+});
